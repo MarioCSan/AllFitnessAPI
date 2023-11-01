8d08158ddf4f4f81fe53d9d2fad695057c706c5e
Mario <canalessanchezmario@gmail.com>
2023-11-01 19:44:31 +0100

    Se crea el modelo del usuario

---
+++ Models/Usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AllFitnessAPI.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int idUsuario {get; set;}
+
+        public string nombre { get; set; }
+        public string email { get; set; }
+        public string nombreUsuario { get; set; }
+        public string imagenUsuario { get; set; }
+        public string password { get; set; }
+
+    }
+}
